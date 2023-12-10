1a819a08ce463708ae5ace3f9d6d718652bc8af9
Yan <zyyang513@gmail.com>
2023-12-09 23:10:44 -0600

    get url link in the page

--- case_tracker/spiders/CaseStatus.py
+++ case_tracker/spiders/CaseStatus.py
@@ -6,7 +6,23 @@ class Casestatus(scrapy.Spider):
     start_urls = ["https://www.casestatusext.com/cases/IOE0918743038/"]
 
     def parse(self, response):
-        cases = response.xpath('//td/a/text()').getall()
+        cases = response.xpath('//td/a')
+        casenames = []
+        links = []
+        for case in cases:
+            casename = case.xpath(".//text()").get()
+            casenames.append(casename)
+            relativelink = case.xpath(".//@href").get()
+
+            # # method 1:
+            # absolutelink = response.urljoin(relativelink)
+            # # method 2:
+            # absolutelink = f'https://www.casestatusext.com/{relativelink}'
+            # method 3:
+            absolutelink = response.follow(relativelink)
+
+            links.append(absolutelink)
         yield {
-            'cases': cases,
+            'casenames': casenames,
+            'links': links,
         }
